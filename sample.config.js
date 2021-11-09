5ae0d2a1ce79a8baed2d97b01ce5fac8e6aad923
Michael Breuer <michael.breuer@aspirecreative.co.uk>
2021-11-09 14:29:02 +0800

    cleanup

--- sample.config.js
+++ sample.config.js
@@ -1,7 +1,7 @@
 const config = () => {
   return {
     envrionment: "development",
-    access_token: "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
+    access_token: "your.mapbox.access.token",
   };
 };
 export default config;
